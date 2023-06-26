d7027a1e319cb0432b68f2872ae37942d188e0a1
MHB <mohelhelb@gmail.com>
2023-06-26 06:19:08 +0200

    Add functionality to profile. Change password. Upload image. Delete account

--- static/js/profile.js
+++ static/js/profile.js
@@ -16,3 +16,53 @@ cancelProfileButton.addEventListener("click", function() {
   // Scroll to the top
   window.scrollTo(0, 0);
 }); 
+
+// Display/Hide Modal
+function displayModal(toggleModal, modal, cancelModal, targetForm) {
+  toggleModal.addEventListener("click", function() {
+    modal.style.display = "block";
+  });
+  cancelModal.addEventListener("click", function() {
+    targetForm.reset();
+    modal.style.display = "none";
+  });
+}
+ 
+// Change Password Modal
+const changePasswordToggle = document.querySelector(".account div:first-child .circle"); 
+changePasswordToggle.addEventListener("click", function () {
+  const changePasswordModal = document.querySelector("#changePasswordModal"); 
+  const cancelChangePasswordModal = document.querySelector("#changePasswordModal button"); 
+  changePasswordModal.style.display = "block";
+  cancelChangePasswordModal.addEventListener("click", function() {  
+    const changePasswordForm = document.querySelector("#changePasswordModal form");
+    changePasswordForm.reset();
+    changePasswordModal.style.display = "none";
+  });
+  window.onclick = function(e) {
+    if (e.target === changePasswordModal) {
+      changePasswordModal.style.display = "none";
+    }
+  }
+});
+
+// Delete Account Modal 
+const deleteAccountToggle = document.querySelector(".account div:last-child .circle"); 
+deleteAccountToggle.addEventListener("click", function () {
+  const deleteAccountModal = document.querySelector("#deleteAccountModal"); 
+  const cancelDeleteAccountModal = document.querySelector("#deleteAccountModal button"); 
+  deleteAccountModal.style.display = "block";
+  cancelDeleteAccountModal.addEventListener("click", function() {  
+    const deleteAccountForm = document.querySelector("#deleteAccountModal form"); 
+    deleteAccountForm.reset();
+    deleteAccountModal.style.display = "none";
+  });
+  window.onclick = function(e) {
+    if (e.target === deleteAccountModal) {
+      deleteAccountModal.style.display = "none";
+    }
+  }
+}); 
+
+
+
